0e9c0bfbf8f34ebde5e97c5998f6fb937e35b61a
2b09 <115679904+2b09@users.noreply.github.com>
2022-10-20 10:49:52 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,51 @@
+let index = 0
+input.onButtonPressed(Button.A, function () {
+    index = 9
+    basic.showNumber(index)
+    while (index > 0) {
+        index += -1
+        basic.showNumber(index)
+    }
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . . . #
+        . . . . .
+        # . . . #
+        . # . # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
